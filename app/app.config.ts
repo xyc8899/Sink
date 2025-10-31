240fc5e6a24298ef58bdb682c6ddc3bbd543ee0e
xyc8899 <156637400+xyc8899@users.noreply.github.com>
2025-10-31 22:12:15 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  title: 'Sink-XYC',
+  email: 'xy188.vip',
+  github: '/',
+  twitter: '/',
+  telegram: '/',
+  mastodon: '/',
+  blog: '/',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
