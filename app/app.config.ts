d8fd8777e6bb7a4af56faccc3d40b9ac569a70a5
xyc8899 <156637400+xyc8899@users.noreply.github.com>
2025-10-31 22:16:26 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   telegram: '',
   mastodon: '',
   blog: '',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'XYC 的安全极速sink短链接服务, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
