d3effb784fffc33b360d3585e53b930ec04be7c8
xyc8899 <156637400+xyc8899@users.noreply.github.com>
2025-10-31 22:14:18 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink-XYC',
-  email: 'xy188.vip',
-  github: '/',
-  twitter: '/',
-  telegram: '/',
-  mastodon: '/',
-  blog: '/',
+  email: 'mail@xy188.vip',
+  github: '',
+  twitter: '',
+  telegram: '',
+  mastodon: '',
+  blog: '',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
